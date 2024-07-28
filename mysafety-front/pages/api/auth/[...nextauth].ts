601070067784d6731bfb8decc11aca62e9def7de
MarkTocino <Mark_tocino@yahoo.com>
2024-07-27 23:23:20 -0400

    Nothing New

--- mysafety-front/pages/api/auth/[...nextauth].ts
+++ mysafety-front/pages/api/auth/[...nextauth].ts
@@ -1,20 +1,18 @@
-
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 
-
 export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-        GithubProvider({ 
-            clientId: process.env.GITHUB_CLIENT_ID as string,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-        })
-    ],
-}
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+  ],
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
