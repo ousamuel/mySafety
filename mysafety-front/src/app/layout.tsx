4486398f97cd5efe38cb86f9b600de110ecdf219
Samuel <samuelou510@gmail.com>
2024-07-28 02:32:32 -0400

    gtag manager

--- mysafety-front/src/app/layout.tsx
+++ mysafety-front/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { authOptions } from "../../pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 import SessionProvider from "../SessionProvider";
 import Navbar from "./components/Navbar";
+import { GoogleTagManager } from '@next/third-parties/google'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,6 +21,10 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions);
   return (
     <html lang="en">
+      <head>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_MEASUREMENT_ID} />
+
+      </head>
         <SessionProvider session={session}>
         <body className={inter.className}>
           <Navbar />
