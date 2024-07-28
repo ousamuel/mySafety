eadad451a4be8c8937b576d561e83209102902f6
Samuel <samuelou510@gmail.com>
2024-07-28 01:48:11 -0400

    /head for GA

--- mysafety-front/src/app/page.tsx
+++ mysafety-front/src/app/page.tsx
@@ -9,7 +9,6 @@ import session from "../app/layout";
 import getOffenders from "./fetches/getOffenders";
 import { Divider } from "@nextui-org/react";
 import GoogleAnalytics from "./components/GoogleAnalytics";
-import Head from "next/head";
 import Google from "next-auth/providers/google";
 
 export default function Home() {
@@ -28,9 +27,9 @@ export default function Home() {
 
   return (
     <>
-      <Head>
+      <head>
         <GoogleAnalytics />
-      </Head>
+      </head>
       <div className="bg-gradient-to-b from-sky-200 to-gray-200 text-gray-800 min-h-screen">
         {/* Navigation Bar */}
 
