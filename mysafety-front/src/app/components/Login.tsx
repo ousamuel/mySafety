e1af7e68ed69cf4d2c20cdbe359039725358275f
MarkTocino <Mark_tocino@yahoo.com>
2024-07-27 20:32:41 -0400

    Almost there with login

--- mysafety-front/src/app/components/Login.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { signIn } from 'next-auth/react'
-export default function loginUser() {
-  return (
-    <div>
-      <button onClick={() => signIn('google')}>LOGIN</button>
-    </div>
-  )
-}
--- mysafety-front/src/app/page.tsx
+++ mysafety-front/src/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import session from "../app/layout";
 // import TrafficForm from "./components/TrafficForm";
 // import RegisteredOffenders from "./components/RegisteredOffenders";
 import getOffenders from "./fetches/getOffenders";
@@ -9,14 +10,10 @@ export default async function Home() {
   // const [trafficData, setTrafficData] = useState<any[]>([]);
   // const [offenderData, setOffenderData] = useState<any>(null);
 
-
-
   // useEffect(() => {
   // }, []);
-  
   return (
     <main>
-      home page page.tsx
       {/* <RegisteredOffenders data={offenderData} /> */}
       {/* <TrafficForm /> */}
     </main>
