650d5d242d7f0cecfb9ec35c9bb4fff296757174
MarkTocino <Mark_tocino@yahoo.com>
2024-07-27 20:32:41 -0400

    Almost there with login

--- mysafety-front/src/SessionProvider.tsx
+++ mysafety-front/src/SessionProvider.tsx
@@ -1,18 +1,14 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { Session } from 'next-auth'
-import { SessionProvider as Provider } from 'next-auth/react'
+import React from "react";
+import { Session } from "next-auth";
+import { SessionProvider as Provider } from "next-auth/react";
 
 type Props = {
-    children: React.ReactNode;
-    session: Session | null;
-}
+  children: React.ReactNode;
+  session: Session | null;
+};
 
-export default function SessionProvider({children, session}: Props) {
-  return (
-    <Provider>
-        {children}
-    </Provider>
-  )
+export default function SessionProvider({ children, session }: Props) {
+  return <Provider>{children}</Provider>;
 }
--- mysafety-front/src/app/Login/page.tsx
+++ mysafety-front/src/app/Login/page.tsx
@@ -1,15 +1,25 @@
-'use client'
-import { useSession, signOut, signIn} from 'next-auth/react'
-import React from 'react'
+"use client";
+import { useSession, signOut, signIn } from "next-auth/react";
+import React from "react";
 export default function login() {
   const session = useSession();
   return (
     <div>
+      <div></div>
       <div>
-      <button onClick={() => signIn('google')}>LOGIN</button>
+        {" "}
+        {session?.data?.user?.name ? (
+          <div>
+            <div>Welcome {session.data.user.name}</div>
+            <button onClick={() => signOut()}>Sign Out</button>
+          </div>
+        ) : (
+          <div>
+            <div>Please Login</div>
+            <button onClick={() => signIn("google")}>LOGIN</button>
+          </div>
+        )}
+      </div>
     </div>
-      <div>{session?.data?.user?.name}</div>
-      <button onClick={() => signOut()}>Sign Out</button>
-    </div>
-  )
+  );
 }
